9cfebc6a80220746d1ebd5e7f1a2beb1ba808131
litao <18772104389@163.com>
2018-12-11 22:56:45 +0800

    定時任務

--- src/main/java/com/springboot/DemoApplication.java
+++ src/main/java/com/springboot/DemoApplication.java
@@ -4,9 +4,10 @@ package com.springboot;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@MapperScan(basePackages = "com.springboot.dao")
-@SpringBootApplication
+@MapperScan(basePackages = {"com.springboot.dao","com.springboot.service.orders"})
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class DemoApplication {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(DemoApplication.class, args);
